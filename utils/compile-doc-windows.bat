316f515b8bc84f2315852f0ca54af78fc105963b
Tobias Verkerk <tobias.verkerk@proton.me>
2025-07-27 00:48:57 +0200

    fix commit SHA in utils
    now takes content SHA rather than the template repo SHA

--- utils/compile-doc-windows.bat
+++ utils/compile-doc-windows.bat
@@ -1,7 +1,7 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-for /f %%i in ('git rev-parse HEAD') do (
+for /f %%i in ('git -C ../../ rev-parse HEAD') do (
     SET "commitHash=%%i"
     typst watch ../main.typ ../../document.pdf --root ../../ --input commit=!commitHash:~0,8!
 )
